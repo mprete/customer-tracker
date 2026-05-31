3991cba67031d481106a15d098d4f928eb6fe671
Mackenzie Prete <mackenzieprete@usf.edu>
2026-05-31 15:04:10 -0400

    Add customer management functionality in cc_3.js

---
+++ cc_3.js
@@ -0,0 +1,33 @@
+const customers = [
+    {
+        name: "Mackenzie Prete",
+        email: "mackenzie.prete@email.com",
+        purchases: ["Laptop", "Mouse", "Monitor"]
+    },
+    {
+        name: "John Smith",
+        email: "John.Smith@email.com",
+        purchases: ["Monitor", "Keyboard", "Mouse"]
+    },
+    {
+        name: "Rachel Johnson",
+        email: "Rachel.Johnson@email.com",
+        purchases: ["Desk", "Desk Lamp", "Charger"]
+    }
+];
+
+customers.push({
+    name: "Matthew Wilson",
+    email: "matthew.wilson@email.com",
+    purchases: ["Phone", "Laptop"]
+    });
+
+customers.shift();
+
+customers[0].email = "johnsmith@email.com";
+
+customers[1].purchases.push("Phone");
+
+customers.forEach(function(customer) {
+    console.log(`Name: ${customer.name}  Email: ${customer.email}  Total Purchases: ${customer.purchases.length}`);
+});
